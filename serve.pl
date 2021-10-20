3076cd26bf16fba31c66a3a9322ce378562ebf94
cailyn baksh <91929105+cailyn-baksh@users.noreply.github.com>
2021-10-20 00:21:14 -0600

    Added MIME type support to serve.pl (goodbye <100 line count ;-;)

--- serve.pl
+++ serve.pl
@@ -12,14 +12,62 @@ sub isPathForbidden {
 	my @subdirs = split('/', $path);
 	my $depth = 0;
 	foreach (@subdirs) {
-		if ($_ eq ".." && $depth == ) {
-			return 0 if ($depth == 0) {  # going above server root
+		if ($_ eq "..") {
+			return 0 if ($depth == 0);  # going above server root
 			--$depth;
 		} else {
 			++$depth if $_ ne "";
 		}
 	}
 	return 1;
+}
+
+sub getMimeType {
+	my ($path) = @_;
+	my %mimeTypes = (  # sorted alphabetically by mime type
+		".json"		=> "application/json",
+		".doc"		=> "application/msword",
+		".ogx"		=> "application/ogg",
+		".pdf"		=> "application/pdf",
+		".rtf"		=> "application/rtf",
+		".ppt"		=> "application/vnd.ms-powerpoint",
+		".pptx"		=> "application/vnd.openxmlformats-officedocument.presentationml.presentation",
+		".docx"		=> "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
+		".xml"		=> "application/xml",
+		".zip"		=> "application/zip",
+		".aac"		=> "audio/aac",
+		".mid"		=> "audio/midi",
+		".midi"		=> "audio/midi",
+		".mp3"		=> "audio/mpeg",
+		".oga"		=> "audio/ogg",
+		".ogg"		=> "audio/ogg",
+		".opus"		=> "audio/opus",
+		".otf"		=> "font/otf",
+		".ttf"		=> "font/ttf",
+		".woff"		=> "font/woff",
+		".woff2"	=> "font/woff2",
+		".apng"		=> "image/apng",
+		".avif"		=> "image/avif",
+		".gif"		=> "image/gif",
+		".jpg"		=> "image/jpeg",
+		".jpeg"		=> "image/jpeg",
+		".jfif"		=> "image/jpeg",
+		".pjpeg"	=> "image/jpeg",
+		".pjp"		=> "image/jpeg",
+		".png"		=> "image/png",
+		".svg"		=> "image/svg",
+		".webp"		=> "image/webp",
+		".ico"		=> "image/x-icon",
+		".css"		=> "text/css",
+		".html"		=> "text/html",
+		".htm"		=> "text/html",
+		".js"		=> "text/javascript",
+		".txt"		=> "text/plain",
+		".mp4"		=> "video/mp4",
+		".mpeg"		=> "video/mpeg",
+		".ogv"		=> "video/ogg"
+	);
+	return %mimeTypes{substr($path, rindex($path, "."), length($path)-1)};
 }
 
 sub main {
@@ -58,7 +106,6 @@ sub main {
 		sendHTTPErrPage($client, 505, "HTTP Version Not Supported") if ($protoVersion ne "HTTP/1.1");
 		sendHTTPErrPage($client, 403, "Forbidden") if (!isPathForbidden($target));
 
-
 		# Collect headers
 		my %headers;
 		while (my $headerLine = <$client>) {
@@ -92,6 +139,8 @@ sub main {
 		print $client "HTTP/1.1 200 OK\r\n";
 
 		# send headers
+		my $mimeType = getMimeType($target);
+		print $client "Content-Type: $mimeType\r\n" if ($mimeType ne "");
 
 		print $client "\r\n";
 
